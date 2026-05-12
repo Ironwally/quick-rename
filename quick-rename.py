fefeac07f86d8c23ffaf23efca8f087a3313d525
Ironwally <ironwally@ratfort.de>
2026-05-12 16:22:12 +0200

    no annoying log file creation

--- quick-rename.py
+++ quick-rename.py
@@ -45,7 +45,8 @@ class RenameOp:
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler(sys.stdout))
-logging.basicConfig(filename='quick-rename.log', level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(filename='quick-rename.log', level=logging.INFO)
 
 def _parse_args(argv: Sequence[str] | None = None) -> argparse.Namespace:
   parser = argparse.ArgumentParser(
